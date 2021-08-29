a7fd2bfc3fd0f051f85e530ea40df3f64c5af93c
arunatebel <arunautebel@gmail.com>
2021-08-29 18:35:37 +0530

    Move oidc provider settings into a common section

--- archeun/settings.py
+++ archeun/settings.py
@@ -129,4 +129,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# oidc_provider related settings
 LOGIN_URL = '/accounts/login/'
+OIDC_USERINFO = 'archeun.oidc_provider_settings.userinfo'
+
