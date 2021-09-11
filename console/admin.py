3ab422b7793012ade7012d5a5f00151b4764dc56
ArunaTebel <arunatebel@hotmail.com>
2021-09-11 20:46:01 +0530

    Add console models to the admin site

--- console/admin.py
+++ console/admin.py
@@ -1,1 +1,65 @@
 """console.admin"""
+from django.contrib import admin
+
+from console.models import Organization, Team, OrganizationOwner, \
+    OrganizationMember, TeamOwner, TeamMember
+
+
+class BookInline(admin.TabularInline):
+    """
+    BookInline
+    """
+    model = Team
+
+
+class OrganizationOwnerInline(admin.TabularInline):
+    """
+    OrganizationOwnerInline
+    """
+    model = OrganizationOwner
+
+
+class OrganizationMemberInline(admin.TabularInline):
+    """
+    OrganizationMemberInline
+    """
+    model = OrganizationMember
+
+
+class TeamOwnerInline(admin.TabularInline):
+    """
+    TeamOwnerInline
+    """
+    model = TeamOwner
+
+
+class TeamMemberInline(admin.TabularInline):
+    """
+    TeamMemberInline
+    """
+    model = TeamMember
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    """
+    OrganizationAdmin
+    """
+    inlines = [
+        BookInline,
+        OrganizationOwnerInline,
+        OrganizationMemberInline,
+    ]
+
+
+class TeamAdmin(admin.ModelAdmin):
+    """
+    TeamAdmin
+    """
+    inlines = [
+        TeamOwnerInline,
+        TeamMemberInline,
+    ]
+
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Team, TeamAdmin)
