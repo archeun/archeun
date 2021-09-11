f643167e49a01b069ca51eaf704ea314c15029d1
ArunaTebel <arunatebel@hotmail.com>
2021-09-11 18:15:20 +0530

    Move views to separate modules

--- console/views/organization.py
+++ console/views/organization.py
@@ -1,18 +1,6 @@
-"""console.views"""
+"""console.views.organizations"""
 from django.shortcuts import render
 from django.views import View
-
-
-class DashboardView(View):
-    """
-    Dashboard view
-    """
-
-    def get(self, request):
-        """
-        Render the dashboard.html
-        """
-        return render(request, 'console/dashboard.html')
 
 
 class OrganizationsView(View):
