05c1ad01f6a01e785545bd8f380b9f5c03fd9f17
ArunaTebel <arunatebel@hotmail.com>
2021-09-11 19:44:08 +0530

    Extend the user model with UserProfile

--- core/models.py
+++ core/models.py
@@ -1,1 +1,14 @@
 """core.models"""
+# pylint: disable=imported-auth-user
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class UserProfile(models.Model):
+    """
+    User profile model by extending the User model
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'arch_core_user_profile'
