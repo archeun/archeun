778c632e2c523f6c43a6e2b66320def9e0bd8b54
ArunaTebel <arunatebel@hotmail.com>
2021-09-12 00:23:39 +0530

    Implement initial CRUD ops for organization model

--- console/services/organization.py
+++ console/services/organization.py
@@ -1,9 +1,11 @@
 """
 Service layer to handle business logic related to Organizations
 """
+# pylint: disable=imported-auth-user
+from django.contrib.auth.models import User
 from django.db.models import QuerySet
 
-from console.models import Organization
+from console.models import Organization, OrganizationOwner
 
 
 def get_user_organizations(user_id: int) -> QuerySet:
@@ -19,3 +21,24 @@ def get_user_organizations(user_id: int) -> QuerySet:
     Raises:
     """
     return Organization.objects.filter(owners__user_id=user_id).all()
+
+
+def add_organization_owner(organization: Organization, user: User) -> Organization:
+    """
+    Adds the employee of the given user as the owner to the given organization
+
+    Arguments:
+        organization (Organization): Organization object to add the owner
+        user (User): User to be added as the owner
+
+    Returns:
+        Organization: The Organization with the owner added
+
+    Raises:
+    """
+    owner = OrganizationOwner()
+    owner.employee = user.employee
+    owner.organization = organization
+    owner.save()
+    organization.organizationowner_set.add(owner)
+    return organization
