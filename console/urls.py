3b29a5f78ed1c3e795a9ec1af968a5666e21f299
ArunaTebel <arunatebel@hotmail.com>
2021-09-12 00:23:39 +0530

    Implement initial CRUD ops for organization model

--- console/urls.py
+++ console/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 
 from console.views.dashboard import DashboardView
-from console.views.organization import OrganizationsView
+from console.views.organization import OrganizationsView, OrganizationCreateView, \
+    OrganizationDetailView, OrganizationUpdateView, OrganizationDeleteView
 
 urlpatterns = [
 
@@ -13,7 +14,27 @@ urlpatterns = [
 
     path(
         'o/',
-        OrganizationsView.as_view(), name='arch-console-orgs'
+        OrganizationsView.as_view(), name='arch-console-org-list'
+    ),
+
+    path(
+        'o/create',
+        OrganizationCreateView.as_view(), name='arch-console-org-create'
+    ),
+
+    path(
+        'o/detail/<int:pk>/',
+        OrganizationDetailView.as_view(), name='arch-console-org-detail'
+    ),
+
+    path(
+        'o/update/<int:pk>/',
+        OrganizationUpdateView.as_view(), name='arch-console-org-update'
+    ),
+
+    path(
+        'o/delete/<int:pk>/',
+        OrganizationDeleteView.as_view(), name='arch-console-org-delete'
     ),
 
 ]
--- console/views/organization.py
+++ console/views/organization.py
@@ -1,8 +1,9 @@
 """console.views.organizations"""
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from console.models import Organization
-from console.services.organization import get_user_organizations
+from console.services.organization import get_user_organizations, add_organization_owner
 
 
 class OrganizationsView(ListView):
@@ -10,8 +11,50 @@ class OrganizationsView(ListView):
     Organizations view
     """
     model = Organization
-    template_name = 'console/organization/organizations.html'
-    context_object_name='organizations'
+    template_name = 'console/organization/list.html'
+    context_object_name = 'organizations'
 
     def get_queryset(self):
         return get_user_organizations(self.request.user.id)
+
+
+class OrganizationCreateView(CreateView):
+    """
+    Create Organizations view
+    """
+    model = Organization
+    fields = ['name', 'description']
+    template_name = 'console/organization/create.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        add_organization_owner(self.object, self.request.user)
+        return response
+
+
+class OrganizationDetailView(DetailView):
+    """
+    Details of Organization view
+    """
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'console/organization/detail.html'
+
+
+class OrganizationUpdateView(UpdateView):
+    """
+    Update Organizations view
+    """
+    model = Organization
+    fields = ['name', 'description']
+    template_name = 'console/organization/update.html'
+
+
+class OrganizationDeleteView(DeleteView):
+    """
+    Delete Organizations view
+    """
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'console/organization/delete.html'
+    success_url = reverse_lazy('arch-console-org-list')
