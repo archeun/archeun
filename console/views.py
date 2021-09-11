bc484d1e99058e5b5dc03ea9d7e2967ce067a127
ArunaTebel <arunatebel@hotmail.com>
2021-09-11 17:57:01 +0530

    Add initial views for console dashboard and organizations

--- console/views.py
+++ console/views.py
@@ -1,1 +1,27 @@
 """console.views"""
+from django.shortcuts import render
+from django.views import View
+
+
+class DashboardView(View):
+    """
+    Dashboard view
+    """
+
+    def get(self, request):
+        """
+        Render the dashboard.html
+        """
+        return render(request, 'console/dashboard.html')
+
+
+class OrganizationsView(View):
+    """
+    Organizations view
+    """
+
+    def get(self, request):
+        """
+        Render the organizations.html
+        """
+        return render(request, 'console/organizations.html')
